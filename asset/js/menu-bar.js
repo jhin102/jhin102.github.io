cc9be0fe0484f9bdc2a79b2b35f0b3256aa371ee
jhin.lee(이상진)/kakaogames <saojin10@naver.com>
2025-10-14 12:12:07 +0900

    스타일 업데이트

--- asset/js/menu-bar.js
+++ asset/js/menu-bar.js
@@ -2,12 +2,12 @@ function insertMenuBar() {
     const menu_bar = document.createElement('div')
     menu_bar.id = 'menu-bar'
     menu_bar.innerHTML = `
-    <a id="logo" href="#"> JHIN .LOG </a>
+    <a id="logo" href="/"> JHIN .LOG </a>
     <div id="menu-container">
         <ul class="menu-list">
-            <li class="memu-item"><a href="/">about</a></li>
-            <li class="memu-item"><a href="/blog.html">blog</a></li>
-            <li class="memu-item"><a href="/projects.html">projects</a></li>
+            <li class="memu-item"><a href="/">ABOUT</a></li>
+            <li class="memu-item"><a href="/blog.html">BLOG</a></li>
+            <li class="memu-item"><a href="/projects.html">PROJECTS</a></li>
         </ul>
     </div>
     `
